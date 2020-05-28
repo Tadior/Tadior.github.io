a6dece74b2e18371741bcde4605edc14d1ce34fc
Tadior <zamulin1998@gmail.com>
2020-05-28 14:05:34 +0300

    te

---
+++ project/js/main.js
@@ -0,0 +1,10 @@
+$('.notification-slider').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 600,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
+
