a7e9408d5406bbbdaa9b0c6ff8d8d402a8099002
Dmitriy <zamulin1998@gmail.com>
2020-12-10 20:05:54 +0300

    First page

--- EnergoTransStroy/#src/js/script.js
+++ EnergoTransStroy/#src/js/script.js
@@ -1,16 +1,16 @@
 //Скрипт для Бургер меню ==========================
 
-//const burger = document.querySelector('.menu__burger');
-//const menuNav = document.querySelector('.menu__nav');
+const burger = document.querySelector('.menu__burger');
+const menuNav = document.querySelector('.menu__nav');
 
-//function show() {
-//   if(burger.classList.contains('show')) {
-//      burger.classList.remove('show');
-//      menuNav.classList.remove('show');
-//   } else {
-//      burger.classList.add('show');
-//      menuNav.classList.add('show');
-//   }
-//}
+function show() {
+   if(burger.classList.contains('show')) {
+      burger.classList.remove('show');
+      menuNav.classList.remove('show');
+   } else {
+      burger.classList.add('show');
+      menuNav.classList.add('show');
+   }
+}
 
-//burger.addEventListener('click', show);
+burger.addEventListener('click', show);
