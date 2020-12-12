2e96a12925111f87863c35193a81bdf2fe39920e
Dmitriy <zamulin1998@gmail.com>
2020-12-12 19:59:21 +0300

    New sections

--- EnergoTransStroy/#src/js/script.js
+++ EnergoTransStroy/#src/js/script.js
@@ -14,3 +14,40 @@ function show() {
 }
 
 burger.addEventListener('click', show);
+
+// Анимация catalog__descr
+
+const catalogDescr = document.querySelectorAll('.catalog__descr');
+const catalogItems = document.querySelectorAll('.catalog__item');
+
+//catalogItems.forEach(item => {
+//   item.addEventListener('mouseover', () => {
+//      //catalogDescr.forEach(descr => {
+//      //   catalogAnimate(descr);
+//      //});
+//      const descr = item.querySelector('.catalog__descr');
+//      catalogAnimate(descr);
+//   });
+//});
+
+//function catalogAnimate(item) {
+//   const element = item;
+//   const start = Date.now(); // Запомнить время начала 
+//   const timer = setInterval(function() {
+//      // сколько времени прошло с начала анимации?
+//      let timePassed = Date.now() - start;
+
+//      if (timePassed >= 2000) {
+//         clearInterval(timer); // закончить анимацию через 2 секунды
+//         return;
+//      }
+
+//      // отрисовать анимацию на момент timePassed, прошедший с начала анимации
+//      draw(timePassed,element);
+//   }, 20);
+//}
+//// в то время как timePassed идёт от 0 до 2000
+//// left изменяет значение от 0px до 400px
+//function draw(timePassed, element) {
+//   element.style.top = timePassed / 20 + '%';
+//}
